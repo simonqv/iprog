1d56de2cd347f887f6ab5924b93dd26aac24077e
Simon Larspers Qvist <simonqv@kth.se>
2023-11-10 11:12:43 +0100

    tw2.3 works now

--- iprog-students_emm2-simonqv-ht23-2_3/tw3.4/src/views/searchFormView.jsx
+++ iprog-students_emm2-simonqv-ht23-2_3/tw3.4/src/views/searchFormView.jsx
@@ -1,25 +1,10 @@
 
 function SearchFormView(props) {
-    // const searchValues = {
-    //     query: props.text ? props.text : "",
-    //     dishType: props.type ? props.type : "",
-    //     setQuery(str) {
-    //         if (str){
-    //             this.query = str
-    //         }
-    //     },
-    //     setDish(dish) {
-    //         if (dish) {
-    //             this.dishType = dish
-    //         }
-    //     }
-    // }
-    
 
     return (
         <div>
-            <input onChange={searchTextACB} defaultValue={props.text ? props.text : ""} ></input>
-            <select onChange={searchTypeACB} defaultValue={props.type ? props.type : ""}>
+            <input onInput={searchTextACB} onChange={setTextACB} value={props.text ? props.text : ""} ></input>
+            <select onInput={searchTypeACB} onChange={setTypeACB} value={props.type ? props.type : ""}>
                 <option value="">Choose:</option>
                 {props.dishTypeOptions.map(dishCB)}
             </select>
@@ -27,21 +12,23 @@ function SearchFormView(props) {
         </div>
     )
 
-    function searchTextACB(evt) {
-        // searchValues.setQuery(evt.target.value)
-        // console.log(searchValues.query)
-        //console.log(props.text)
+    function setTextACB(evt) {
         props.onSearchTextACB(evt.target.value)
     }
 
+    function searchTextACB(evt) {
+        props.onSearchTextACB(evt.target.value)
+    }
+
+    function setTypeACB(evt) {
+        props.onTypeChangeACB(evt.target.value)
+    }
+
     function searchTypeACB(evt) {
-        // searchValues.setDish(evt.target.value)
-        // console.log(searchValues.dishType)
         props.onTypeChangeACB(evt.target.value)
     }
 
     function searchACB(evt) {
-        // console.log("search vals" , searchValues.dishType, searchValues.query)
         props.onClickSearchACB()
     }
 
