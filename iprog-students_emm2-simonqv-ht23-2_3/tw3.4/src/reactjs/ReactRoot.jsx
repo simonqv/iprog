ace4fbbf7a5d55957f11adeab237071311f12aa9
Simon Larspers Qvist <simonqv@kth.se>
2023-11-11 19:20:47 +0100

    last task done

--- iprog-students_emm2-simonqv-ht23-2_3/tw3.4/src/reactjs/ReactRoot.jsx
+++ iprog-students_emm2-simonqv-ht23-2_3/tw3.4/src/reactjs/ReactRoot.jsx
@@ -1,12 +1,24 @@
 import SideBar from "../reactjs/sidebarPresenter.jsx";
 import Summary from "./summaryPresenter.jsx";
+import Search from "../reactjs/searchPresenter.jsx";
+import Details from "../reactjs/detailsPresenter.jsx";
+import { useEffect } from "react";
 
 export default
 // observer(     will be added in week 3
 function ReactRoot(props){
+    //useEffect(() => {
+        // Do initial search
+    //    props.model.doSearch({});
+    //}, []); // The empty dependency array ensures that this effect runs only once after the initial render
+      
     return (<div>
                 <div><SideBar model={props.model}/></div>
-                <div><Summary model={props.model}/></div>
+                <div>
+                    <Search model={props.model}/>
+                    <Details model={props.model}/>
+                    <Summary model={props.model}/>
+                </div>
             </div>
            );
 }
