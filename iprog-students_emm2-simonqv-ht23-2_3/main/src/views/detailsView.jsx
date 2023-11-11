89bed56d192fc73c6a76cbffc060e0a4b3bad6d6
Emm Nilsson <emmnilsson@n153-p53.eduroam.kth.se>
2023-11-11 13:34:23 +0100

    Styled detail view

--- iprog-students_emm2-simonqv-ht23-2_3/main/src/views/detailsView.jsx
+++ iprog-students_emm2-simonqv-ht23-2_3/main/src/views/detailsView.jsx
@@ -11,27 +11,26 @@ import { menuPrice } from "/src/utilities.js";
 function DetailsView(props) {
    return (
     <div>
-        <div>{props.dishData.title}</div>
+        <div className="title">{props.dishData.title}</div>
         <div className="rowC">
             <img src={props.dishData.image} height="100"></img>
-            <p>
+            <p style={{margin: "25px 10px"}}>
                 Price: {  props.dishData.pricePerServing}
                 <br/>
                 for {props.guests} guests: {(props.dishData.pricePerServing * props.guests).toFixed(2)}
             </p>
         </div>
-        <table>
+        <table style={{margin: "10px 0 0 0", border: "2px solid"}}>
             <tbody>
                 {props.dishData.extendedIngredients.map(ingredientsCB)}
             </tbody>
         </table>
         {console.log(props.dishData)}
         {console.log(props)}
-        {props.dishData.instructions}
+        <p style={{margin: "10px 0"}}>{props.dishData.instructions}</p>
+        <a href={props.dishData.sourceUrl} target="_blank" >More information</a>
         <br/>
-        <a href={props.dishData.sourceUrl} target="_blank">More information</a>
-        <br/>
-        <button disabled={props.isDishInMenu} onClick={addToMenuACB}>Add to menu!</button>
+        <button disabled={props.isDishInMenu} onClick={addToMenuACB} style={{margin: "10px 15px 0px 0px"}}>Add to menu!</button>
         <button>Cancel</button>
     </div>
    )
